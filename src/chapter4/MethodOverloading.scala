9ec8172c80a04dd8ffcabb79254d0462d1ddf0d4
Gulbrandsen, Geir <ggulbrandsen@ebay.com>
2016-09-04 11:01:22 +0100

    Same same, but different

---
+++ src/chapter4/MethodOverloading.scala
@@ -0,0 +1,15 @@
+
+def addNum(x:Int) = x + 1
+def addNum(x:Double) = x + 30.0
+def addNum(x:String) = x + 19
+
+
+println(addNum(30))
+println(addNum(40.0))
+println(addNum("Hello "))
+
+def addTen(x:Int) = x + 10
+def addTen(x:Double) = x + 10.0
+
+println(addTen(5))
+println(addTen(6.0))
